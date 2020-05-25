5f6a8a90d07e9a791c41bc7528653b660774cf7d
harveyjones01 <harveyirl@yahoo.co.uk>
2020-05-25 11:28:13 +0100

    inserting/finetune nav bar

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ function myFunction(x) {
 
   x.classList.toggle("change");
 
+  //var y = document.getElementById("navigation-bar");
+  //if (y.style.display === "block") {
+  //  y.style.display = "none";
+  //} else {
+  //  y.style.display = "block";
+  //}
+
   var y = document.getElementById("navigation-bar");
-  if (y.style.display === "block") {
-    y.style.display = "none";
+  if (y.style.visibility === "visible") {
+    y.style.visibility = "hidden";
   } else {
-    y.style.display = "block";
+    y.style.visibility = "visible";
   }
 
 }

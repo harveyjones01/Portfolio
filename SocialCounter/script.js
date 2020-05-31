49e914adbfb63003d5af5cf6a3112a00bb923cc1
harveyjones01 <harveyirl@yahoo.co.uk>
2020-05-31 11:23:14 +0100

    inserting weather app

--- SocialCounter/script.js
+++ SocialCounter/script.js
@@ -2,30 +2,26 @@
 
 
 
-function getFollowers(){
-  console.log('epic');
-  const puppeteer = require('puppeteer');
-  async function scrapeProduct(url){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+$.ajax({
+  type: "POST",
+  url: ".\pyscript.py",
+  data: { param: text}
+  }).done(function(o) {
+      console.log(data);
+      console.log(text);
+  });
 
-    const [el] = await page.$x('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span');
-    const src = await el.getProperties('src');
-    const srcTxt = await src.jsonValue();
 
-    console.log('epic2');
-    alert({srcTxt});
+
+  function runPython(){
+    alert("pressed")
+    $.ajax({
+      type: "get",
+      url: ".\pyscript.py",
+      cache:false,
+      async:'asynchronous'
+      }).done(function(o) {
+          console.log(data);
+          console.log(text);
+      });
   }
-
-  scrapeProduct('https://www.instagram.com/harvey_jones2001/');
-  
-}
-
-
-function lol(){
-  const uniqid = require('uniqid');
-  document.querySelector('#result').innerHTML = uniqid();
-}
-
-

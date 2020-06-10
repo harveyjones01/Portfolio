d9976a7ac711b4b42bd1dc11c209aa2ba9ce1611
harveyjones01 <harveyirl@yahoo.co.uk>
2020-06-10 12:18:59 +0100

    addding about me page

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 function myFunction(x) {
 
   x.classList.toggle("change");
-
-  //var y = document.getElementById("navigation-bar");
-  //if (y.style.display === "block") {
-  //  y.style.display = "none";
-  //} else {
-  //  y.style.display = "block";
-  //}
 
   var y = document.getElementById("navigation-bar");
   if (y.style.visibility === "visible") {
@@ -15,6 +9,7 @@ function myFunction(x) {
   } else {
     y.style.visibility = "visible";
   }
-
 }
+
+
 
